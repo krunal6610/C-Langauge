11242b3650ec57fcada188bbe4f20086327c2d15
krunal6610 <120013579+krunal6610@users.noreply.github.com>
2022-12-07 15:42:06 +0530

    Add files via upload

---
+++ PRINT_NAME.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int
+main ()
+{
+
+  printf ("Name : Krunal Parekh\n");
+  printf ("Birth Date : 09/01/2001\n");
+  printf ("Age : 21\n");
+  printf ("Address : Ahmedabad ");
+
+
+  return 0;
+
+}
