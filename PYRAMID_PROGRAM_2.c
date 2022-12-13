1b4515c826749da8ddfd167eba2f5423681ed554
krunal6610 <120013579+krunal6610@users.noreply.github.com>
2022-12-13 16:37:15 +0530

    Add files via upload

---
+++ PYRAMID_PROGRAM_2.c
@@ -0,0 +1,36 @@
+/*
+
+ * * * * * 
+ * * * * 
+ * * * 
+ * * 
+ *
+
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+
+  for (int i = 0; i < 5; i++)
+    {
+
+
+      for (int j = 5; j > i; j--)
+	{
+
+
+	  printf ("*");
+
+
+	}
+
+      printf ("\n");
+
+    }
+
+  return 0;
+}
+
+
