b1b3a79ea6fb9ce21cb4cb4a97c1b51edb42f8bc
krunal6610 <120013579+krunal6610@users.noreply.github.com>
2022-12-21 15:31:44 +0530

    Add files via upload

---
+++ FIND_NUM_FROM_POS_ARRAY.c
@@ -0,0 +1,93 @@
+/*
+
+  Program of find the element of given position from the array.
+
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+  int arr[5];
+  int num, p, count = 0, count2 = 0;
+
+  printf ("enter the 5 elements: \n\n");
+
+  for (int i = 0; i < 5; i++)
+    {
+
+      printf ("enter element at position [%d] : ", i);
+      scanf ("%d", &arr[i]);
+
+
+    }
+
+  printf ("\n\n------NUM_TO_POS------\n\n");
+  printf ("\nenter the num to find it position: ");
+  scanf ("%d", &num);
+
+
+  for (int i = 0; i < 5; i++)
+    {
+
+      if (num == arr[i])
+	{
+
+	  count = i;
+
+	  //scanf("%d",&arr[i]);
+
+	}
+
+    }
+
+  if (count == 0)
+    {
+
+      printf ("enter valid num\n");
+
+
+    }
+  else
+    {
+
+      printf ("position is [%d]\n", count);
+    }
+
+
+
+  printf ("\n\n------POS_TO_NUM------\n\n");
+
+  printf ("\nenter the position to find the element: ");
+  scanf ("%d", &p);
+
+  for (int i = 0; i < 5; i++)
+    {
+
+      if (arr[i] == arr[p])
+	{
+
+	  count2 = i;
+
+
+	}
+
+
+    }
+
+  if (count2 == 0)
+    {
+
+      printf ("enter valid position\n");
+
+    }
+  else
+    {
+
+      printf ("\nAt position[%d] num is: %d", p, arr[p]);	//count2,arr[count2]
+
+    }
+
+
+  return 0;
+}
